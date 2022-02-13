d265fb27954b7390a8d8675e630faf3ee9392092
Enzo <Enzo_seg@outlook.com>
2022-02-13 20:42:39 -0300

    Added test suit for character api (not working yet)

--- app/src/test/java/com/example/starwarsencyclopedia/CharactersApiServiceTests.kt
+++ app/src/test/java/com/example/starwarsencyclopedia/CharactersApiServiceTests.kt
@@ -1,2 +1,43 @@
 package com.example.starwarsencyclopedia
 
+import com.example.starwarsencyclopedia.network.characterapi.CharacterApiService
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import junit.framework.Assert.*
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class CharactersApiServiceTests : BaseTest() {
+
+    private lateinit var service: CharacterApiService
+
+    @Before
+    fun setup() {
+        val url = mockWebServer.url("/")
+        service = Retrofit.Builder()
+            .baseUrl(url)
+            .addConverterFactory(
+                MoshiConverterFactory.create(
+                    Moshi.Builder()
+                        .add(KotlinJsonAdapterFactory())
+                        .build()
+                ))
+            .build()
+            .create(CharacterApiService::class.java)
+    }
+
+    @Test
+    fun api_service() {
+        enqueue("star_wars_characters.json")
+        runBlocking {
+            val apiResponse = service.getFirstPage()
+
+            assertNotNull(apiResponse)
+            //assertTrue("The list was empty", apiResponse.isNotEmpty())
+            //assertEquals("The id's did not match", "424905", apiResponse[0].id)
+        }
+    }
+}
